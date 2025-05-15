0a179c8cd9e0acd4764edab23e01d134d018259a
toktobek-01 <toktobek.d@gmail.com>
2025-05-14 20:57:38 -0500

    new mistakes

---
+++ src/ChessboardPattern.java
@@ -0,0 +1,17 @@
+public class ChessboardPattern {
+    public static void main(String[] args) {
+        int size = 8;
+
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                if ((row + col) % 2 == 0) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
