e132c6b51d2ecd5810ed21a4f0032c017487c05f
toktobek-01 <toktobek.d@gmail.com>
2025-06-09 20:50:08 -0500

    new mistakes

--- src/practiceGpt/Vehicles/MainV.java
+++ src/practiceGpt/Vehicles/MainV.java
@@ -1,24 +1,36 @@
 package practiceGpt.Vehicles;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainV {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        UserVehicles vehicles = new UserVehicles();
+        ArrayList<UserVehicles> vehiclesList = new ArrayList<>();
 
-        System.out.println("Enter vehicle name: ");
-        String name = scanner.nextLine();
-        vehicles.setName(name);
+        System.out.println("How many vehicles do you want to add? ");
+        int count = scanner.nextInt();
+        scanner.nextLine();
 
-        System.out.println("Enter fuel type(e.g., gasoline, diesel, electric): ");
-        String fuel = scanner.nextLine();
-        vehicles.setFuelType(fuel);
+        for (int i = 0; i < count; i++) {
+            System.out.println("\nVehicles #" + (i + 1));
+            UserVehicles vehicles = new UserVehicles();
+            System.out.println("Enter vehicle name: ");
+            String name = scanner.nextLine();
+            vehicles.setName(name);
+
+            System.out.println("Enter fuel type(e.g., gasoline, diesel, electric): ");
+            String fuel = scanner.nextLine();
+            vehicles.setFuelType(fuel);
+            vehiclesList.add(vehicles);
+        }
 
         System.out.println();
-        vehicles.showInfo();
-        vehicles.start();
-        vehicles.stop();
+        for (UserVehicles v : vehiclesList) {
+        v.showInfo();
+        v.start();
+        v.stop();
+    }
 
         scanner.close();
     }
